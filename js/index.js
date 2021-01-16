3e768c656bd005917e888ceedcb7455a719fd215
mabel <bigbelk@live.ru>
2021-01-16 18:57:43 +0300

    src-from-search-location

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-const asciiPlayer = document.querySelector("asciinema-player")
+const asciiHolder = document.querySelector("section")
+const locHash = window.location.search
+const mediaSrc = locHash ? locHash.substring(1) : "index"
+
+console.log(mediaSrc)
+
+asciiHolder.innerHTML = `<asciinema-player src="media/${mediaSrc}.cast" cols="100" rows="26" id="asciinema" font-size=".8rem" preload="auto" speed="2.8"></asciinema-player>`
+
+const asciiPlayer = asciiHolder.querySelector("asciinema-player")
 
 asciiPlayer.addEventListener("canplaythrough", ()=> {
     const controlBarOrig = asciiPlayer.querySelector(".control-bar")
@@ -6,7 +14,7 @@ asciiPlayer.addEventListener("canplaythrough", ()=> {
     controlBarOrig.parentNode.appendChild(controlBarNew)
     controlBarOrig.style.display = "none"
     controlBarNew.classList.add("control-bar")
-    controlBarNew.innerHTML = '<audio src="media/index.mp3" controls preload="auto"></audio>'
+    controlBarNew.innerHTML = `<audio src="media/${mediaSrc}.mp3" controls preload="auto"></audio>`
     const audioPlayer = document.querySelector("audio")
     asciiPlayer.addEventListener("play",  () => audioPlayer.play())
     asciiPlayer.addEventListener("pause", () => audioPlayer.pause())
